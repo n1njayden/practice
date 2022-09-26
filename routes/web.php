a9b08226bc2fa8781bcc340062d42803104ae398
n1njayden <94377575+n1njayden@users.noreply.github.com>
2022-09-26 14:53:33 +0200

    modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('posts', [
+        $posts = Post::latest();
+
+        if(request('search')){
+            $posts->where('title', 'like', '%' . request('search') . '%');
+        }
+
+        return view('posts', [
         'posts' => Post::latest()->get(),
         'categories' => Category::all()
+
+
     ]);
 })->name('home');
 
